733ee80b6585a72d457df1675c99f52608245607
Bhaskar <15045469+8h45k4r@users.noreply.github.com>
2026-03-11 01:48:04 +0545

    Update __init__.pyfeat: register owasp_agentic, owasp_api, mitre_atlas, dod_ai in frameworks __init__

--- sentinel/compliance/frameworks/__init__.py
+++ sentinel/compliance/frameworks/__init__.py
@@ -18,6 +18,10 @@ from sentinel.compliance.frameworks.hipaa import HIPAAFramework
 from sentinel.compliance.frameworks.iso27001 import ISO27001Framework
 from sentinel.compliance.frameworks.owasp_llm import OWASPLLMFramework
 from sentinel.compliance.frameworks.soc2 import SOC2Framework
+from sentinel.compliance.frameworks.owasp_agentic import OWASPAgenticFramework
+from sentinel.compliance.frameworks.owasp_api import OWASPAPIFramework
+from sentinel.compliance.frameworks.mitre_atlas import MITREATLASFramework
+from sentinel.compliance.frameworks.dod_ai import DoDAlFramework
 
 FRAMEWORK_REGISTRY: dict[str, BaseFramework] = {
     "eu_ai_act": EUAIActFramework(),
@@ -27,18 +31,43 @@ FRAMEWORK_REGISTRY: dict[str, BaseFramework] = {
     "china_ai_regs": ChinaAIRegsFramework(),
     "oecd_principles": OECDPrinciplesFramework(),
     "ieee7000": IEEE7000Framework(),
-            "hipaa": HIPAAFramework(),
+    "hipaa": HIPAAFramework(),
     "iso27001": ISO27001Framework(),
     "owasp_llm": OWASPLLMFramework(),
     "soc2": SOC2Framework(),
+    "owasp_agentic": OWASPAgenticFramework(),
+    "owasp_api": OWASPAPIFramework(),
+    "mitre_atlas": MITREATLASFramework(),
+    "dod_ai": DoDAlFramework(),
 }
+
+ALL_FRAMEWORKS = [
+    EUAIActFramework,
+    ISO42001Framework,
+    NISTAIRMFFramework,
+    GDPRFramework,
+    ChinaAIRegsFramework,
+    OECDPrinciplesFramework,
+    IEEE7000Framework,
+    HIPAAFramework,
+    ISO27001Framework,
+    OWASPLLMFramework,
+    SOC2Framework,
+    OWASPAgenticFramework,
+    OWASPAPIFramework,
+    MITREATLASFramework,
+    DoDAlFramework,
+]
 
 __all__ = [
     "BaseFramework", "Control", "ControlStatus", "EvidenceRecord",
     "FrameworkMetadata", "FrameworkStatus", "FRAMEWORK_REGISTRY",
+    "ALL_FRAMEWORKS",
     "EUAIActFramework", "ISO42001Framework", "NISTAIRMFFramework",
     "GDPRFramework", "ChinaAIRegsFramework", "OECDPrinciplesFramework",
     "IEEE7000Framework",
-        "HIPAAFramework", "ISO27001Framework", "OWASPLLMFramework",
+    "HIPAAFramework", "ISO27001Framework", "OWASPLLMFramework",
     "SOC2Framework",
+    "OWASPAgenticFramework", "OWASPAPIFramework", "MITREATLASFramework",
+    "DoDAlFramework",
 ]
